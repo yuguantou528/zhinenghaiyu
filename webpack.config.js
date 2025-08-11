000a3f606088c2241a566af449007f9f22597500
yuguantou528 <971446815@qq.com>
2025-08-11 13:45:39 +0800

    修复Vercel部署问题：使用相对路径和contenthash

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     publicPath: './',
     clean: true
   },
